ff59576f1ef32d475933f66c0fc35bb60ac52e59
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-18 16:12:02 +0100

    can return word count test and methods written

---
+++ app/src/main/java/example/codeclan/com/wordcounter/WordCounter.java
@@ -0,0 +1,19 @@
+package example.codeclan.com.wordcounter;
+
+/**
+ * Created by user on 18/04/2017.
+ */
+
+public class WordCounter {
+
+    private String sentence;
+
+    public WordCounter(String sentence){
+        this.sentence = sentence;
+    }
+
+
+    public String getWordCount() {
+        return sentence;
+    }
+}
--- app/src/test/java/example/codeclan/com/wordcounter/WordCounterTest.java
+++ app/src/test/java/example/codeclan/com/wordcounter/WordCounterTest.java
@@ -1,12 +1,21 @@
 package example.codeclan.com.wordcounter;
 
+import org.junit.Before;
+
 import static junit.framework.Assert.assertEquals;
 
 /**
  * Created by user on 18/04/2017.
  */
 
-public class WordCounter {
+public class WordCounterTest {
+
+    WordCounter wordCounter;
+
+    @Before
+    public void before(){
+        wordCounter = new WordCounter("This is my first sentence");
+    }
 
     public void canGetWordCount(){
         assertEquals("This is my first sentence", wordCounter.getWordCount());

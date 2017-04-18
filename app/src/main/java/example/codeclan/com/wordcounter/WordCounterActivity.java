d8a33449afeb7af1ada72a52ed44bd9879133282
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-18 18:02:24 +0100

    app now takes in words and returns the word count

--- app/src/main/java/example/codeclan/com/wordcounter/WordCounterActivity.java
+++ app/src/main/java/example/codeclan/com/wordcounter/WordCounterActivity.java
@@ -2,12 +2,33 @@ package example.codeclan.com.wordcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class WordCounterActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private EditText wordsEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_counter);
+
+        wordsEditText = (EditText) findViewById(R.id.sentence_text);
+        textView = (TextView) findViewById(R.id.word_count_text);
     }
+
+    public void onButtonClicked(View button){
+        String sentenceThatWasGiven = wordsEditText.getText().toString();
+
+        WordCounter wordCounter = new WordCounter(sentenceThatWasGiven);
+        int wordCount = wordCounter.getWordCount();
+
+        textView.setText("The count was: " + wordCount);
+
+    }
+
 }
